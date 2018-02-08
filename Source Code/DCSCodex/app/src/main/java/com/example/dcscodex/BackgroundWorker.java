25b4e8b9c45084a3bcb0b3271c560af04954a85c
Kim Borja <krborja@up.edu.ph>
2018-02-08 21:53:15 +0800

    Added Java Source Code

---
+++ Source Code/DCSCodex/app/src/main/java/com/example/dcscodex/BackgroundWorker.java
@@ -0,0 +1,253 @@
+package com.example.dcscodex;
+
+/*
+* Created by Kim Borja
+* This is a course requirement for CS 192 Software Engineering II under the supervision of
+* Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering,
+* University of the Philippines, Diliman for the AY 2017-2018”.
+*/
+
+
+/*
+* Code History
+*
+* Name of Programmer            Date changed                Description
+* Borja, Kim                    01/31/18                    Created the BackgroundWorker  class
+*                                                           Added doInBackground method
+*
+* Borja, Kim                    02/01/16                    Created onPreExecute, onPostExecute, and onProgressUpdate method        
+*
+* Borja, Kim                    02/02/16                    Edited if-else statements under onPostExecute method         
+*
+* Borja, Kim                    02/03/18                    Integrated the LoginActivity and RegisterActivity class with
+*                                                           BackgroundWorker
+*
+* Borja, Kim                    02/07/18                    Added necessary comments
+*
+*/
+
+
+/*
+* File creation date: 01/31/18
+* Development group: Group 4
+* Client Group: Students
+* Purpose: BackgroundWorker class contains the doInBackground method which handles the database part of the app
+*          during user login and registration. This class handles the user login input and registration input for
+*          the database to process through PHP scripts.
+*
+*/
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+
+
+
+public class BackgroundWorker extends AsyncTask<String,Void,String> {
+     Context context;
+     AlertDialog alertDialog;
+
+     BackgroundWorker (Context ctx) {
+
+          context = ctx; /* assign context to local context */
+    }
+
+
+     /*
+     * Method Name: doInBackground
+     * Creation date: 01/31/18
+     * Purpose: Checks the first parameter passed to BackgroundWorker. If first paramater
+     *          is equal to "login", it proceeds to the login procedure. Otherwise, it
+     *          proceeds to the register procedure. This handles the php script responsible
+     *          for the connection to the database.
+     * Required Files: LoginActivity.java, RegisterActivity.java, connection.php, login_dcscodex.php, register_dcscodex.php
+     * Return value: String message echoed from the php script
+     */
+
+     @Override
+     protected String doInBackground(String... params) {
+
+          /* expect the value of the first element which is type="login" */
+          String type = params[0];                                                    /* catches the constant string flag for login passed as the first parameter to BackgroundWorker */
+          String login_url = "http://192.168.254.111/login_dcscodex.php";             /* variable for the PHP script location used for login (database connection) */
+          String register_url = "http://192.168.254.111/register_dcscodex.php";       /* variable for the PHP script location used for registration (database connection) */
+          /* always check and update the ip address of the pc which is currently in use since the app might crash */
+
+          if (type.equals("login")) {
+               try {
+
+                    /* Get the values from the passed parameters from LoginActivity.java */
+
+                    String user_id = params[1];         /* catches the student number passed as parameter from LoginActivity */
+                    String password = params[2];        /* catches the password passed as parameter from LoginActivity */
+
+                    URL url = new URL(login_url);       /* for login_url variable defined above*/
+                    HttpURLConnection httpURLConnection =  (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("POST");
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                    /* data to be added in the database */
+                    String post_data = URLEncoder.encode("user_id", "UTF-8")+"="+URLEncoder.encode(user_id, "UTF-8")+"&"
+                                   +URLEncoder.encode("password", "UTF-8")+"="+URLEncoder.encode(password, "UTF-8");
+                    bufferedWriter.write(post_data);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                    String result="";  /* stores the echoed message from the php script */
+                    String line="";    /* for reading lines echoed from php script */
+
+                    while((line = bufferedReader.readLine()) != null) {
+                         result += line;
+                         /* message from the server side is stored in result; it contains message echoed from the php file */
+                    }
+
+                    bufferedReader.close();
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    return result;
+
+
+               } catch (MalformedURLException e) {
+                    e.printStackTrace();
+               } catch (IOException e) {
+                    e.printStackTrace();
+               }
+
+          } else if (type.equals("register")) {
+               try {
+                
+                    /* Get the values from the passed parameters from RegisterActivity.java */
+                
+                    String id = params[1];                      /* catches the student number passed as parameter from RegisterActivity */
+                    String surname = params[2];                 /* catches the surname passed as parameter from RegisterActivity */
+                    String password = params[3];                /* catches the password passed as parameter from RegisterActivity */
+                    String confirm_password = params[4];        /* catches the confirmed password passed as parameter from RegisterActivity */
+
+                    URL url = new URL(register_url);
+                    HttpURLConnection httpURLConnection =  (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("POST");
+                    httpURLConnection.setDoOutput(true);
+                    httpURLConnection.setDoInput(true);
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                    /* data to be added in the database */
+                    String post_data = URLEncoder.encode("id", "UTF-8")+"="+URLEncoder.encode(id, "UTF-8")
+                                   + "&"+URLEncoder.encode("surname", "UTF-8")+"="+URLEncoder.encode(surname, "UTF-8")
+                                   + "&"+URLEncoder.encode("password", "UTF-8")+"="+URLEncoder.encode(password, "UTF-8")
+                                   + "&"+URLEncoder.encode("confirm_password", "UTF-8")+"="+URLEncoder.encode(confirm_password, "UTF-8");
+                    bufferedWriter.write(post_data);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
+                    String result="";   /* stores the echoed message from the php script */
+                    String line="";     /* for reading lines echoed from php script */
+
+                    while((line = bufferedReader.readLine()) != null) {
+                         result += line;
+                         /* message from the server side is stored in result; it contains message echoed from the php file */
+                    }
+
+                    bufferedReader.close();
+                    inputStream.close();
+                    httpURLConnection.disconnect();
+                    return result;
+
+
+               } catch (MalformedURLException e) {
+                    e.printStackTrace();
+               } catch (IOException e) {
+                    e.printStackTrace();
+               }
+          }
+          return null;
+     }
+
+
+     /*
+     * Method Name: onPreExecute
+     * Creation date: 02/01/16
+     * Purpose: Creates alert dialog and displays the message "Status"
+     * Return value: void
+     */
+
+     @Override
+     protected void onPreExecute() {
+          alertDialog = new AlertDialog.Builder(context).create();
+          alertDialog.setTitle("Status");
+     }
+
+
+     /*
+     * Method Name: onPostExecute
+     * Creation date: 02/01/16
+     * Purpose: Prompts user to check input based on
+     *          the message echoed from the php script
+     * Return value: void
+     */
+
+     @Override
+     protected void onPostExecute(String result) {
+          alertDialog.setMessage(result);
+          alertDialog.show();
+          /* show message which is coming from the server */
+          /* the String result here contains messages echoed from the php file */
+          /* after login displays result */
+
+          if (result.length() == 17) {
+               /* "Login successful" */
+               context.startActivity(new Intent(context, CalendarActivity.class));
+          } else if (result.length() == 19){
+               /* "Login unsuccessful" */
+               Toast.makeText(context, "Wrong Student Number or Password", Toast.LENGTH_SHORT).show();
+          } else if (result.length() == 18) {
+               /* "Insert successful" */
+               context.startActivity(new Intent(context, LoginActivity.class));
+          } else {
+               Toast.makeText(context, "Registration unsuccessful", Toast.LENGTH_SHORT).show();
+          }
+
+
+
+
+
+
+
+     }
+
+
+     /*
+     * Method Name: onProgressUpdate
+     * Creation date: 02/01/16
+     * Purpose: Progress update
+     * Return value: void
+     */
+
+     @Override
+     protected void onProgressUpdate(Void... values) {
+          super.onProgressUpdate(values);
+     }
+
+}
---
+++ Source Code/DCSCodex/app/src/main/java/com/example/dcscodex/InternetConnection.java
@@ -0,0 +1,64 @@
+package com.example.dcscodex;
+
+
+/*
+* Created by Kim Borja
+* This is a course requirement for CS 192 Software Engineering II under the supervision of
+* Asst. Prof. Ma. Rowena C. Solamo of the Department of Computer Science, College of Engineering,
+* University of the Philippines, Diliman for the AY 2017-2018”.
+*/
+
+
+/*
+* Code History
+*
+* Name of Programmer            Date changed                Description
+* Borja, Kim                    01/31/18                    Created the InternetConnection  class
+*
+* Borja, Kim                    02/07/18                    Added necessary comments
+*
+*/
+
+
+/*
+* File creation date: 01/31/18
+* Development group: Group 4
+* Client Group: Students
+* Purpose: InternetConnection class checks whether the user's
+*          phone has internet or not.
+*
+*/
+
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+
+
+public class InternetConnection {
+
+
+     /*
+     * Method Name: checkConnection
+     * Creation date: 01/31/18
+     * Purpose: Checks whether internet connection is available or not
+     * Return value: boolean; 1 if there's internet connection; otherwise, 0
+     */
+
+     /* Check whether internet connection is available or not */
+     public static boolean checkConnection(Context context) {
+          final ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+          NetworkInfo activeNetworkInfo = connMgr.getActiveNetworkInfo();
+
+          if (activeNetworkInfo != null) { /* connected to internet */
+               if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_WIFI) {     /* connected to wifi */
+                    return true;
+               } else if (activeNetworkInfo.getType() == ConnectivityManager.TYPE_MOBILE) {     /* connected to mobile data */
+                    return true;
+               }
+          }
+          return false;
+     }
+}
